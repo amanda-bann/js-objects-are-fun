fd3cf57163078e2e1ee697fe21cbe58532228305
amanda-bann <amanda_klap@protonmail.com>
2024-11-08 11:10:22 -0700

    Create an object & properties using dot notation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs", "Understand JS Syntax", "JS APIs", "JS Libraries"];
+learning.category = "Frontend Development";
+learning.topicImportance = "High";
+
+console.log(learning);
