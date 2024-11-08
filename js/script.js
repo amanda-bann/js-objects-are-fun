23de87af2851499b9310300328e318fd9fd80031
amanda-bann <amanda_klap@protonmail.com>
2024-11-08 11:25:02 -0700

    Change & remove values

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ const learning = {};
 
 learning.topic = "JS";
 learning.learningGoals = ["Build JS Programs", "Understand JS Syntax", "JS APIs", "JS Libraries"];
-learning.category = "Frontend Development";
+learning.category = "Front End Development";
 learning.topicImportance = "High";
+
+// console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1, 1);
 
 console.log(learning);
