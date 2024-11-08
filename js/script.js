14df16b4270eda8a0080f7009bc205ec4a86490c
amanda-bann <amanda_klap@protonmail.com>
2024-11-08 11:34:16 -0700

    Display text & remove a class

--- js/script.js
+++ js/script.js
@@ -14,3 +14,9 @@ learning.topic = "JavaScript";
 learning.learningGoals.splice(1, 1);
 
 console.log(learning);
+
+topicElement.innerText = `✔️ I'm learning ${learning.topic}`;
+topicElement.classList.remove("hide");
+
+countElement.innerText = `✔️ I have ${learning.learningGoals.length} learning goals`;
+countElement.classList.remove("hide");
